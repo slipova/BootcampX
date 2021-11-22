4f3824daf68fb49e2a8d02a9e20506cb17499402
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-22 21:25:29 +0000

    add data to assignments and assignment submissions

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,17 +1,16 @@
 CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
-  name TEXT NOT NULL,
+  name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE
 );
 
-
 CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
-  name TEXT NOT NULL,
-  email TEXT,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
   phone VARCHAR(32),
-  github TEXT,
+  github VARCHAR(255),
   start_date DATE,
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
