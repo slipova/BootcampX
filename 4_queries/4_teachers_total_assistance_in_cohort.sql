3afbdbb3d0ea3778de2d4bddb8a02b9cede6b8c0
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-23 23:11:45 +0000

    refactor

--- 4_queries/4_teachers_total_assistance_in_cohort.sql
+++ 4_queries/4_teachers_total_assistance_in_cohort.sql
@@ -1,20 +1,3 @@
--- SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
--- FROM teachers
--- JOIN assistance_requests ON teachers.id = teacher_id
--- JOIN students ON students.id = student_id
--- JOIN cohorts ON cohorts.id = cohort_id
--- WHERE cohorts.name = 'JUL02'
--- ORDER BY teacher;
-
-
--- -- all teachers all cohorts
--- SELECT DISTINCT teachers.name AS teacher, COUNT(assistance_requests) as requests
--- FROM teachers
--- JOIN assistance_requests ON teachers.id = teacher_id
--- GROUP BY teachers.name
--- ORDER BY teacher;
-
-
 
 SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) as requests
 FROM teachers
@@ -22,6 +5,6 @@ JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON student_id  = students.id
 JOIN cohorts ON cohort_id  = cohorts.id
 WHERE cohorts.name = 'JUL02'
--- GROUP BY cohorts.name
+
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
