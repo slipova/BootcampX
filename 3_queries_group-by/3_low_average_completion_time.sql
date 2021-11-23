8cb5788bef4cfca184b2173a62a3e6dc1d3dee47
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-23 01:44:12 +0000

    add queries group by

---
+++ 3_queries_group-by/3_low_average_completion_time.sql
@@ -0,0 +1,8 @@
+SELECT students.name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration, AVG(assignments.duration) AS average_estimated_duration
+FROM students
+JOIN assignment_submissions ON assignment_submissions.student_id = students.id
+JOIN assignments ON assignments.id = assignment_submissions.assignment_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
+ORDER BY AVG(assignment_submissions.duration);
---
+++ 3_queries_group-by/3_students_by cohort.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name AS cohort_name, COUNT(*) AS student_count
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING COUNT(*) >= 18
+ORDER BY COUNT(*);
