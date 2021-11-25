5659fb4dc32fb7c4f48735914e83b60b166b1912
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-25 03:01:45 +0000

    connect server to database

---
+++ bootcamp_app/students.js
@@ -0,0 +1,53 @@
+const { Pool } = require('pg');
+const cohortInput = process.argv[2];
+const limitInput = process.argv[3];
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'vagrant'
+});
+
+
+//RETURNS TABLE-LIKE OBJECT
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `)
+//   .then(res => {
+//     console.log(res.rows);
+//   })
+//   .catch(err => console.error('query error', err.stack));
+
+//RETURNS A PHRASE
+// pool.query(`
+// SELECT students.id, students.name, cohorts.name AS cohort_name
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// GROUP BY students.id, cohorts.name
+// LIMIT 5;
+// `)
+//   .then(res => {
+//     res.rows.forEach((user) => {
+//       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+//     })
+//   });
+
+//
+pool.query(`
+  SELECT students.id, students.name, cohorts.name AS cohort_name
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  GROUP BY students.id, cohorts.name
+  HAVING cohorts.name LIKE '${cohortInput}%'
+  LIMIT ${limitInput};
+  `)
+  .then(res => {
+    res.rows.forEach((user) => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+    })
+  });
+
+
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,46 @@
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'vagrant'
+});
+
+
+// pool.query(
+//   `SELECT teachers.name, cohorts.name AS cohort, COUNT(assistance_requests) as requests
+// FROM teachers
+// JOIN assistance_requests ON teachers.id = teacher_id
+// JOIN students ON student_id  = students.id
+// JOIN cohorts ON cohort_id  = cohorts.id
+// WHERE cohorts.name = '${process.argv[2]}'
+
+// GROUP BY teachers.name, cohorts.name
+// ORDER BY teachers.name;
+// `)
+//   .then(res => {
+//     res.rows.forEach((teacher) => {
+//       console.log(`${teacher.cohort}: ${teacher.name}`)
+//     })
+//   });
+
+
+pool.query(
+  `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) as requests
+  FROM teachers
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON student_id  = students.id
+  JOIN cohorts ON cohort_id  = cohorts.id
+  WHERE cohorts.name = '${process.argv[2]}'
+  
+  GROUP BY teachers.name, cohorts.name
+  ORDER BY teachers.name;
+  `)
+  .then(res => {
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.name}`)
+    })
+  });
+
